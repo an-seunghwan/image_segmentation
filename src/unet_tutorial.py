dcd563db579fdc3f6901218cf6efaff02f8b8e83
an-seunghwan <dpeltms79@gmail.com>
2021-05-07 13:44:23 +0900

    update

--- src/unet_tutorial.py
+++ src/unet_tutorial.py
@@ -38,19 +38,49 @@ data_gen_args = dict(rotation_range=0.2,
                     horizontal_flip=True,
                     fill_mode='nearest')
 #%%
-batch_size = 20
-train_path = 'data/membrane/train'
-image_folder = 'image'
-mask_folder = 'label'
-aug_dict = data_gen_args
-image_color_mode = "grayscale"
-mask_color_mode = "grayscale"
-image_save_prefix = "image"
-mask_save_prefix  = "mask"
-# num_class = 2
-save_to_dir = "data/membrane/train/aug"
-target_size = (256, 256)
-seed = 1
+# batch_size = 20
+# train_path = 'data/membrane/train'
+# image_folder = 'image'
+# mask_folder = 'label'
+# aug_dict = data_gen_args
+# image_color_mode = "grayscale"
+# mask_color_mode = "grayscale"
+# image_save_prefix = "image"
+# mask_save_prefix  = "mask"
+# save_to_dir = "data/membrane/train/aug"
+# target_size = (256, 256)
+# seed = 1
+#%%
+# image_datagen = ImageDataGenerator(**data_gen_args)
+# mask_datagen = ImageDataGenerator(**data_gen_args)
+
+# image_generator = image_datagen.flow_from_directory(
+#     train_path,
+#     classes = [image_folder],
+#     class_mode = None,
+#     color_mode = image_color_mode,
+#     target_size = target_size,
+#     batch_size = batch_size,
+#     save_to_dir = save_to_dir,
+#     save_prefix  = image_save_prefix,
+#     seed = seed)
+
+# mask_generator = mask_datagen.flow_from_directory(
+#     train_path,
+#     classes = [mask_folder],
+#     class_mode = None,
+#     color_mode = mask_color_mode,
+#     target_size = target_size,
+#     batch_size = batch_size,
+#     save_to_dir = save_to_dir,
+#     save_prefix  = mask_save_prefix,
+#     seed = seed)
+
+# train_generator = zip(image_generator, mask_generator)
+
+# sampleimg, samplemask = next(iter(train_generator))
+# print(sampleimg.shape)
+# print(samplemask.shape)
 #%%
 def BuildTrainGenerator(batch_size,
                     train_path,
@@ -89,6 +119,7 @@ def BuildTrainGenerator(batch_size,
         save_to_dir = save_to_dir,
         save_prefix  = mask_save_prefix,
         seed = seed)
+    
     train_generator = zip(image_generator, mask_generator)
     
     for (img, mask) in train_generator:
@@ -109,46 +140,59 @@ for i, batch in enumerate(traingenerator):
         break
 #%%
 def unet(pretrained_weights = None, 
-         input_size = (256, 256, 1)):
+        input_size = (256, 256, 1)):
+    
+    '''contracting path'''
     inputs = Input(input_size)
     conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
     conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) # 128x128x64
+    
     conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
     conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) # 64x64x128
+    
     conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
     conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3) # 32x32x256
+    
     conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
     conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
-    drop4 = Dropout(0.5)(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+    drop4 = Dropout(0.5)(conv4) # 32x32x512, augmentation
+    
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4) # 16x16x512
 
+    '''bottle-neck'''
     conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
     conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
-    drop5 = Dropout(0.5)(conv5)
+    drop5 = Dropout(0.5)(conv5) # 16x16x1024, augmentation
 
-    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
-    merge6 = concatenate([drop4,up6], axis = 3)
+    '''expanding path'''
+    updrop5 = UpSampling2D(size = (2, 2))(drop5) # 32x32x1024
+    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(updrop5) 
+    merge6 = concatenate([drop4, up6], axis = 3)
     conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
     conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
 
-    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
-    merge7 = concatenate([conv3,up7], axis = 3)
+    upconv6 = UpSampling2D(size = (2, 2))(conv6) # 64x64x512
+    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(upconv6)
+    merge7 = concatenate([conv3, up7], axis = 3)
     conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
     conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
 
-    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
-    merge8 = concatenate([conv2,up8], axis = 3)
+    upconv7 = UpSampling2D(size = (2, 2))(conv7) # 128x128x256
+    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(upconv7)
+    merge8 = concatenate([conv2, up8], axis = 3)
     conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
     conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
 
-    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
-    merge9 = concatenate([conv1,up9], axis = 3)
+    upconv8 = UpSampling2D(size = (2, 2))(conv8) # 256x256x128
+    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(upconv8)
+    merge9 = concatenate([conv1, up9], axis = 3)
     conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
     conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9) # 256x256x2, final feature map
+    
     conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
 
     model = Model(inputs, conv10)
@@ -166,46 +210,26 @@ model = unet()
 model_checkpoint = ModelCheckpoint('./assets/unet_membrane.hdf5', monitor='loss', verbose=1, save_best_only=True)
 model.fit(traingenerator, steps_per_epoch=2000, epochs=5, callbacks=[model_checkpoint])
 #%%
-def testGenerator(test_path,num_image = 30,target_size = (256,256),flag_multi_class = False,as_gray = True):
+def BuildTestGenerator(test_path,
+                        num_image = 30,
+                        target_size = (256, 256),
+                        as_gray = True):
     for i in range(num_image):
-        img = io.imread(os.path.join(test_path,"%d.png"%i),as_gray = as_gray)
-        img = img / 255
-        img = trans.resize(img,target_size)
-        img = np.reshape(img,img.shape+(1,)) if (not flag_multi_class) else img
-        img = np.reshape(img,(1,)+img.shape)
+        img = io.imread(os.path.join(test_path, "%d.png"%i), as_gray = as_gray)
+        img = img / 255.0
+        img = trans.resize(img, target_size)
+        img = np.reshape(img, img.shape + (1,))
+        # img = np.reshape(img, (1,)+img.shape)
         yield img
 #%%
-def geneTrainNpy(image_path,mask_path,flag_multi_class = False,num_class = 2,image_prefix = "image",mask_prefix = "mask",image_as_gray = True,mask_as_gray = True):
-    image_name_arr = glob.glob(os.path.join(image_path,"%s*.png"%image_prefix))
-    image_arr = []
-    mask_arr = []
-    for index,item in enumerate(image_name_arr):
-        img = io.imread(item,as_gray = image_as_gray)
-        img = np.reshape(img,img.shape + (1,)) if image_as_gray else img
-        mask = io.imread(item.replace(image_path,mask_path).replace(image_prefix,mask_prefix),as_gray = mask_as_gray)
-        mask = np.reshape(mask,mask.shape + (1,)) if mask_as_gray else mask
-        img,mask = adjustData(img,mask,flag_multi_class,num_class)
-        image_arr.append(img)
-        mask_arr.append(mask)
-    image_arr = np.array(image_arr)
-    mask_arr = np.array(mask_arr)
-    return image_arr,mask_arr
+def saveResult(save_path, npyfile):
+    for i, item in enumerate(npyfile):
+        img = item[:,:,0]
+        io.imsave(os.path.join(save_path,"predict_{}.png".format(i)), img)
 #%%
-def labelVisualize(num_class,color_dict,img):
-    img = img[:,:,0] if len(img.shape) == 3 else img
-    img_out = np.zeros(img.shape + (3,))
-    for i in range(num_class):
-        img_out[img == i,:] = color_dict[i]
-    return img_out / 255
-#%%
-def saveResult(save_path,npyfile,flag_multi_class = False,num_class = 2):
-    for i,item in enumerate(npyfile):
-        img = labelVisualize(num_class,COLOR_DICT,item) if flag_multi_class else item[:,:,0]
-        io.imsave(os.path.join(save_path,"%d_predict.png"%i),img)
-#%%
-testGene = testGenerator("data/membrane/test")
+testgenerator = BuildTestGenerator("data/membrane/test")
 model = unet()
 model.load_weights("unet_membrane.hdf5")
-results = model.predict_generator(testGene,30,verbose=1)
-saveResult("data/membrane/test",results)
+results = model.predict_generator(testgenerator, 30, verbose=1)
+saveResult("data/membrane/test", results)
 #%%
